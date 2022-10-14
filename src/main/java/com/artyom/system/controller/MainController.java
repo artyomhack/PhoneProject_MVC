01b1a689fa75453aeff38d199cb90038a3cfe1a5
Artyom <xakimullin2@gmail.com>
2022-10-14 23:29:18 +0300

    Init commit

--- src/main/java/com/artyom/system/controller/MainController.java
+++ src/main/java/com/artyom/system/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.artyom.system.controller;
 
+import com.artyom.system.service.user.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
 
     @GetMapping("/")
-    public String getIndex() {
-        return "index";
+    public ModelAndView getMainPage() {
+        var model = new ModelAndView();
+
+        model.setViewName("index");
+        return model;
     }
 }
