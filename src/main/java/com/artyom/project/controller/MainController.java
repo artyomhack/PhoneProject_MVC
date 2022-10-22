73fb6e9c71bf7dc49c2a436724326ad68cf3cf37
Artyom <xakimullin2@gmail.com>
2022-10-22 22:57:37 +0300

    Init commit

--- src/main/java/com/artyom/project/controller/MainController.java
+++ src/main/java/com/artyom/project/controller/MainController.java
@@ -1,10 +1,15 @@
 package com.artyom.project.controller;
 
+import com.artyom.project.model.phone.PhoneNumberId;
+import com.artyom.project.service.phone.PhoneDetails;
 import com.artyom.project.service.phone.PhoneInteractorImpl;
 import com.artyom.project.service.user.UserInteractor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/")
@@ -25,8 +30,35 @@ public class MainController {
 
     @PostMapping("/{id}")
     public ModelAndView getUser(@PathVariable String id) {
-        return null;
+        var phone = interaction.fetchById(PhoneNumberId.of(id));
+
+        return showPhonePage(phone);
     }
 
+    @GetMapping("/createPhone")
+    public ModelAndView showCreatePhone() {
+        return showPhonePage(null);
+    }
 
+    @PostMapping(value = "/createPhone", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ModelAndView createPhone(PhoneDetails details) {
+        var phone = interaction.fetchById(interaction.add(details));
+
+        if (Objects.isNull(phone))
+            return showPhonePage(phone);
+
+        return new ModelAndView("redirect:/createPhone/" + phone.getId());
+    }
+
+    private ModelAndView showPhonePage(PhoneDetails details) {
+        var model = new ModelAndView();
+
+        if (Objects.nonNull(details)) {
+            model.getModelMap().addAttribute("id", details.getId());
+            model.getModelMap().addAttribute("number", details.getNumber());
+        }
+
+        model.setViewName("form_phone");
+        return model;
+    }
 }
